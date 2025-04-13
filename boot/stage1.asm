b301c693055049df883ada0db9abfa03b23e7421
Dioque <eduardodioque@gmail.com>
2025-04-13 17:33:57 -0300

    correção pois o boot não estava sendo inicializado

--- boot/stage1.asm
+++ boot/stage1.asm
@@ -2,20 +2,21 @@
 [ORG 0x7C00]
 
 call LoadSystem
-jmp 0800h:0000h
 
 LoadSystem:
-    mov ah, 02h    ; Função de leitura de disco
-    mov al, 5      ; Número de setores (ajuste conforme necessário)
-    mov ch, 0      ; Cilindro 0
-    mov cl, 2      ; Setor 2 (o bootloader está no setor 1)
-    mov dh, 0      ; Cabeçote 0
-    mov dl, 80h    ; Disco rígido (80h = primeiro HD)
-    mov bx, 0800h  ; Segmento ES (onde o kernel será carregado)
+    mov ah, 0x02         ; Função de leitura de disco
+    mov al, 10           ; Número de setores (ajuste conforme necessário)
+    mov ch, 0            ; Cilindro 0
+    mov cl, 10           ; Setor 10 (onde o segundo bootloader está)
+    mov dh, 0            ; Cabeçote 0
+    mov dl, 0x80         ; Disco rígido
+    mov bx, 0800h        ; Segmento ES (onde o segundo bootloader será carregado)
     mov es, bx
-    mov bx, 0000h  ; Offset 0 (ES:BX = 0800h:0000h)
+    mov bx, 0000h        ; Offset 0 (ES:BX = 0800h:0000h)
     int 13h
-    ret
+
+    ; Salta para o segundo bootloader que foi carregado para 0x0800:0x0000
+    jmp 0800h:0000h
 
 times 510 - ($-$$) db 0
 dw 0xAA55
